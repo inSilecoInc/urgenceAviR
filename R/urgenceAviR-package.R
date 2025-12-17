3594412b9f6d28af4a6f5e01cc541bd3dbf02e2b
SteveViss <svissault01@gmail.com>
2025-12-17 15:04:35 -0500

    Move globals and set visible bindings stats

--- R/urgenceAviR-package.R
+++ R/urgenceAviR-package.R
@@ -1,21 +1,26 @@
 #' @keywords internal
 "_PACKAGE"
-
-## usethis namespace: start
 #' @import shiny
-#' @importFrom graphics image legend
-#' @importFrom stats complete.cases rnorm runif
-## usethis namespace: end
-NULL
 
-# Global variables used in data.table operations
+# Global variables used throughout the package
 utils::globalVariables(c(
   ".data",
-  "groupe_fonctionnel",
-  "nomfr",
-  "nomla",
-  "milieu_marin",
-  "nom_francais",
-  "nom_latin",
-  "inv_type"
+  "Alpha", "Alpha_Code", "An", "Annee", "AnneeDebut", "CODE_ID", "CentroideX", "CentroideY",
+  "Code", "Code4_EN", "Code4_FR", "CodeSp", "Count", "Date", "Espece", "Groupe_FR", "Jour",
+  "JourDebut", "Lat", "LatDec", "LatStart", "Latin", "Latitude", "Long", "LongDec", "LongStart",
+  "Longitude", "Mois", "MoisDebut", "N", "Name_EN", "Name_SC", "NomCol", "NomFR", "Nom_FR",
+  "Nom_Scient", "Nombre", "Observateur", "Observateurs", "ObserverName", "Region",
+  "STATUT_COS", "SousGroupe", "Species", "Species_ID", "abondance", "alpha_code", "annee",
+  "category", "code4_fr", "code_id", "code_sp", "day", "inconnus", "jour", "latitude", "loc_ID",
+  "longitude", "methode", "mois", "month", "n_obs", "nb_nicheur", "no_seance", "nomRef", "nom_fr",
+  "obs", "read.csv", "read.csv2", "setNames", "species_id", "visuelblancs", "visuelbruns", "year",
+  "Nom_Ile", "Nom_francais",
+  "Nb_compte", "Methode_descriptif","Obs_ID","Nb_total_ind",
+  "CruiseID","StartDate","LatStart" ,"LongStart",
+  "Alpha","Count","Distance","InTransect",
+  "SeaState","FlySwim",
+  "Latdec", "Londec", "ColonyID", "Platform", "Colony_size", "Data_location", "Species_code",
+  "Observer1", "Observer2", "Source", "ColonyId", "locality", "code_fr", "taxonomy",
+  "Unite", "Espece_code", "ObsLat", "ObsLong", "Observer", "alpha",
+  "groupe_fonctionnel", "nomfr", "nomla", "milieu_marin", "nom_francais", "nom_latin", "inv_type"
 ))
--- R/zzz.R
+++ R/zzz.R
@@ -1,26 +1,6 @@
 # Package environment for storing mutable state
 .pkg_env <- new.env(parent = emptyenv())
 
-globalVariables(c(
-  "Alpha", "Alpha_Code", "An", "Annee", "AnneeDebut", "CODE_ID", "CentroideX", "CentroideY",
-  "Code", "Code4_EN", "Code4_FR", "CodeSp", "Count", "Date", "Espece", "Groupe_FR", "Jour",
-  "JourDebut", "Lat", "LatDec", "LatStart", "Latin", "Latitude", "Long", "LongDec", "LongStart",
-  "Longitude", "Mois", "MoisDebut", "N", "Name_EN", "Name_SC", "NomCol", "NomFR", "Nom_FR",
-  "Nom_Scient", "Nombre", "Observateur", "Observateurs", "ObserverName", "Region",
-  "STATUT_COS", "SousGroupe", "Species", "Species_ID", "abondance", "alpha_code", "annee",
-  "category", "code4_fr", "code_id", "code_sp", "day", "inconnus", "jour", "latitude", "loc_ID",
-  "longitude", "methode", "mois", "month", "n_obs", "nb_nicheur", "no_seance", "nomRef", "nom_fr",
-  "obs", "read.csv", "read.csv2", "setNames", "species_id", "visuelblancs", "visuelbruns", "year",
-  "Nom_Ile", "Nom_francais",
-  "Nb_compte", "Methode_descriptif","Obs_ID","Nb_total_ind",
-  "CruiseID","StartDate","LatStart" ,"LongStart",
-  "Alpha","Count","Distance","InTransect",
-  "SeaState","FlySwim",
-  "Latdec", "Londec", "ColonyID", "Platform", "Colony_size", "Data_location", "Species_code",
-  "Observer1", "Observer2", "Source", "ColonyId", "locality", "code_fr", "taxonomy",
-  "Unite", "Espece_code", "ObsLat", "ObsLong", "Observer", "alpha"
-))
-
 # List of external files with file names and required column names
 .pkg_env$external_files <- list(
   canards_de_mer = list(
